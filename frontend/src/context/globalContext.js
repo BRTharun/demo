54358f3012236faee424aca7e61d5ea88165f54f
BRTharun <79271916+BRTharun@users.noreply.github.com>
2025-02-13 21:12:33 +0530

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+const BASE_URL = "http://13.60.193.221:5000";
 
 
 const GlobalContext = React.createContext()
